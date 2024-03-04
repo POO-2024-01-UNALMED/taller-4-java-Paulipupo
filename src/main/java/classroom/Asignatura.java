856542342243cd2e7ea0b19ec5920244a1bb8498
Paulipupo <mpupom@unal.edu.co>
2024-03-04 18:50:41 -0500

    Primer intento

--- src/main/java/classroom/Asignatura.java
+++ src/main/java/classroom/Asignatura.java
@@ -2,18 +2,16 @@ package classroom;
 
 public class Asignatura {
 
-    public String nombre;
-    String n = nombre;
-    public int codigoInterno;
-    public int codigoExterno;
-    public Tipo tipo;
+    String nombre;
+    private int codigoInterno;
+    int codigoExterno;
+    private Tipo tipo;
 
     public Asignatura() {
         this("Sin nombre",Tipo.FUNDAMENTACION);
-        this(0);
     }
 
-    public Asignatura(int codigoInterno,Tipo tipo) {
+    public Asignatura(Tipo tipo,int codigoInterno) {
         this("Sin nombre", codigoInterno, 0,tipo);
     }
 
@@ -39,9 +37,9 @@ public class Asignatura {
         this.tipo = tipo;
     }
 
-    public void cambiarDatos(int codigoInterno) {
+    /*public void cambiarDatos(int codigoInterno) {
         this.codigoInterno = codigoInterno;
-    }
+    }*/
 
     public void cambiarDatos(int codigoExterno) {
         this.codigoExterno = codigoExterno;
--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -3,37 +3,42 @@ package classroom;
 public class Persona {
 
     final long cedula;
-    String nombre;
-    static int totalPersonas;
+    private String nombre;
+    private static int totalPersonas;
     
     static {
         totalPersonas = 0;
-        cedula = 3;
+        /*cedula = 3;  /* No se puede escribir aquí cédula porque es un atributo de instancia y final */
     }
 
     public Persona(long cedula, String nombre) {
         this.cedula = cedula;
         this.nombre = nombre;
-        totalPersonas++;
+        Persona.totalPersonas++; /* Llamar desde la clase */
     }
     
     public Persona(String nombre, long cedula) {
         this.cedula = cedula;
         this.nombre = nombre;
-        totalPersonas++;
+        Persona.totalPersonas++; /* Llamar desde la clase */
     }
 
     public Persona(long cedula) {
         this.cedula = cedula;
         this.nombre = "";
-        totalPersonas++;
+        Persona.totalPersonas++; /* Llamar desde la clase */
     }
 
     public Persona(String nombre) {
-        this.nombre = "";
-        totalPersonas++;
+        this.nombre = nombre; /* Asignar el valor de nombre al atributo nombre */
+        this.cedula = 1; /* cedula = 0 */
+        Persona.totalPersonas++; /* Llamar desde la clase */
     }
     
+    public Persona() {  /*Crear constructor vacío y asignar valores vacios */
+        this(0,"");
+    }
+
     public long getCedula() {
         return cedula;
     }
