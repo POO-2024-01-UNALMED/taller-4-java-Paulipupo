db9fe2f06e834d9accd88a75a693e19c29d24463
Paulipupo <mpupom@unal.edu.co>
2024-03-04 18:58:41 -0500

    Segundo intento
    Cambio accesibilidad

--- src/main/java/classroom/Asignatura.java
+++ src/main/java/classroom/Asignatura.java
@@ -2,10 +2,10 @@ package classroom;
 
 public class Asignatura {
 
-    String nombre;
-    private int codigoInterno;
-    int codigoExterno;
-    private Tipo tipo;
+	public String nombre;
+    public int codigoInterno;
+    public int codigoExterno;
+    public Tipo tipo;
 
     public Asignatura() {
         this("Sin nombre",Tipo.FUNDAMENTACION);
